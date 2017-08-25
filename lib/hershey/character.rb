d73993e3e5327aedb9cb5efb1b7997ad837d7d57
Winston Durand <me@winstondurand.com>
2017-08-25 13:24:14 -0700

    *sigh*

--- lib/hershey/character.rb
+++ lib/hershey/character.rb
@@ -27,6 +27,7 @@ module Hershey
     private
 
     def info
+      @@chars = {}
       @@char_list ||= YAML.load_file(File.join(File.dirname(__FILE__), 'fonts_full.yaml'))[@font]
       @@chars[@char] ||= @@char_list[@char.to_s.ord - OFFSET]
       unless @@chars[@char]
